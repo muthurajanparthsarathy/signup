17ec75addead4a561fe34a02d5393c7b39115b42
muthurajanparthsarathy <118988315+muthurajanparthsarathy@users.noreply.github.com>
2022-12-19 23:06:18 +0530

    Add files via upload

---
+++ signup.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en" class="h-100">
+
+<head>
+
+    <title>CODE WITH HOSSEIN</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap v5.1.3 CDNs -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+
+    <!-- CSS File -->
+    <link rel="stylesheet" href="../css/styles.css">
+<style>
+    body
+    {
+        background:url(../images/university.jpg);
+        background-repeat: no-repeat;
+  background-size: 100%;
+  height:100vh;
+  opacity: 90%;
+  width:100%;
+  background-attachment:fixed;
+    }
+    </style>
+</head>
+
+<body >
+
+    <div class="login">
+
+        <h1 class="text-center">Sign In</h1>
+        
+        <form class="needs-validation ">
+            <div class="form-group was-validated">
+                <label class="form-label" for="email">Email address</label>
+                <input class="form-control" type="email" id="email" required>
+                <div class="invalid-feedback">
+                    Please enter your email address
+                </div>
+            </div>
+            <div class="form-group was-validated">
+                <label class="form-label" for="password">Password</label>
+                <input class="form-control" type="password" id="password" required>
+                <div class="invalid-feedback">
+                    Please enter your password
+                </div>
+            </div>
+            <div class="form-group form-check">
+                <input class="form-check-input" type="checkbox" id="check">
+                <label class="form-check-label" for="check">Remember me</label>
+            </div>
+            <input class="btn btn-success w-100" type="submit" value="SIGN IN">
+        </form>
+
+    </div>
+
+</body>
+
+</html>
